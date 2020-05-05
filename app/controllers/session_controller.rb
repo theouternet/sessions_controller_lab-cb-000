ef55cecb2723db538a63ce27acb983392d913f26
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-05 05:24:26 +0000

    Automatically backed up by Learn

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         
       else 
         session[:name] = params[:name]
-        redirect_to '/'
+        redirect_to root_path
     end
   end 
     
